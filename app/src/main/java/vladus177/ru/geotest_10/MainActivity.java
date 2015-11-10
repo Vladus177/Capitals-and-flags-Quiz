0969d5f17deffacd6fb7da8d5d1fabe4a8338909
Vladislav Rusakov <RV123@mail.ru>
2015-11-10 14:52:22 +0300

    *Tablet orientation *Ru language

--- app/src/main/java/vladus177/ru/geotest_10/MainActivity.java
+++ app/src/main/java/vladus177/ru/geotest_10/MainActivity.java
@@ -5,12 +5,14 @@ import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 
 
 public class MainActivity extends Activity {
@@ -19,6 +21,7 @@ public class MainActivity extends Activity {
     FragmentButtons fragButtons;
     FragmentTransaction fTrans;
     FrameLayout container;
+    FrameLayout container2;
     FrameLayout containerButtons;
     FragmentManager myFragmentManager;
 
@@ -30,10 +33,11 @@ public class MainActivity extends Activity {
         fragC=new FragmentCapitals();
         fragButtons=new FragmentButtons();
         container = (FrameLayout) findViewById(R.id.container);
+        container2 = (FrameLayout) findViewById(R.id.container2);
         containerButtons = (FrameLayout) findViewById(R.id.containerButtons);
         myFragmentManager = getFragmentManager();
         addFragmentButtons();
-        addFragmentFlags();
+        //addFragmentFlags();
 
     }
 
@@ -61,16 +65,37 @@ public class MainActivity extends Activity {
     public void addFragmentFlags()
     {
         fTrans=myFragmentManager.beginTransaction();
-        fragF = new FragmentFlags();
-        fTrans.replace(R.id.container,fragF);
-        fTrans.commit();
+        Configuration config = getResources().getConfiguration();
+        if (config.orientation == Configuration.ORIENTATION_PORTRAIT)
+        {
+            fragF = new FragmentFlags();
+            fTrans.replace(R.id.container, fragF);
+            fTrans.commit();
+        }
+        else if(config.orientation == Configuration.ORIENTATION_LANDSCAPE)
+        {
+            fragF = new FragmentFlags();
+            fTrans.replace(R.id.container2,new FragmentFlags());
+            fTrans.commit();
+        }
     }
     public void addFragmentCapitals()
     {
         fTrans=myFragmentManager.beginTransaction();
-        fragC = new FragmentCapitals();
-        fTrans.replace(R.id.container,fragC);
-        fTrans.commit();
+        Configuration config = getResources().getConfiguration();
+        if (config.orientation == Configuration.ORIENTATION_PORTRAIT)
+        {
+            fragC = new FragmentCapitals();
+            fTrans.replace(R.id.container, fragC);
+            fTrans.commit();
+        }
+        else if(config.orientation == Configuration.ORIENTATION_LANDSCAPE)
+        {
+            fragC = new FragmentCapitals();
+            fTrans.replace(R.id.container, new FragmentCapitals());
+            fTrans.commit();
+        }
+
     }
     public void addFragmentButtons()
     {
@@ -79,7 +104,5 @@ public class MainActivity extends Activity {
         fTrans.replace(R.id.containerButtons,fragButtons);
         fTrans.commit();
     }
-
-
 
 }
